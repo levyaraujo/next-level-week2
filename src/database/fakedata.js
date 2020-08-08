43dd549eb187fdff27e9203b6b6980dc6b3d6bec
Levy Araujo <levya95@gmail.com>
2020-08-08 16:09:05 -0300

    database

---
+++ src/database/fakedata.js
@@ -0,0 +1,30 @@
+// data
+const proffys = [
+    {
+      name: "Levy Araújo",
+      avatar:
+        "https://avatars0.githubusercontent.com/u/59183140?s=460&u=faa22588ce1c1d27be2a50d9d211915d90c32c0e&v=4",
+      whatsapp: "94988049873",
+      bio:
+        "Apaixonado por programação. Louco por aprender as melhores técnicas e estilos para entregar aquilo que o usuário necessita. Ensino sempre visando o aprendizado de resolver problemas",
+      subject: "Programação",
+      cost: "50",
+      weekday: [0],
+      time_from: [720],
+      time_to: [1220],
+    },
+  
+    {
+      name: "Diego Fernandes",
+      avatar:
+        "https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4",
+      whatsapp: "2199053378",
+      bio:
+        "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma de minhas explosões",
+      subject: "Química",
+      cost: "20",
+      weekday: [3],
+      time_from: [800],
+      time_to: [2100],
+    },
+  ];
